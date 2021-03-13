3a055c3d8f443223cbc18216646204b149f18e36
moinabyssinia <michaelg.tadesse@gmail.com>
2021-03-13 15:33:27 -0500

    header complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Particles from "react-particles-js";
 import Navbar from "./components/NavBar";
 import Header from "./components/Header";
 
 function App() {
   return (
     <>
+    <Particles 
+    className = "particles-canvas"
+      params = {{
+        particles: {
+          number: {
+            value: 50,
+            density: {
+              enable: true,
+              value_area: 900
+            }
+          },
+          shape:  {
+            type: "square",
+            stroke: {
+              width: 6,
+              color: '#f9ab00'
+            }
+          }
+        }
+      }}
+
+    />
     <Navbar />
     <Header />
     </>
