c8a7350ec2cd05a7cfb039349ba366ec2dbfcf76
moinabyssinia <michaelg.tadesse@gmail.com>
2021-03-18 21:13:18 -0400

    added merch contents

--- src/components/Merchandise.js
+++ src/components/Merchandise.js
@@ -12,27 +12,42 @@ function Merchandise() {
 
             <div className = "container merch-wrapper">
                 <div className = "row">
-                    <div className = "col-lg-4 col-xm-12">
+                    <div className = "col-lg-4 col-xm-12 d-flex justify-content-between">
+                    <div className = "box">
+                            <h3>Books</h3>
+                            <div className = "merch-photo-wrap mb">
+                                <img className = "merch-item" src = {books} alt = "cds"/>
+                            </div>
 
-                        <div className = "photo-wrap mb-5">
-                            <img className = "merch-item" src = {books} alt = "books"/>
+                        </div>
+
+                    </div>
+
+                    <div className = "col-lg-4 col-xm-12">
+                    <div className = "box">
+                            <h3>Paintings</h3>
+                            <div className = "merch-photo-wrap mb">
+                                <img className = "merch-item" src = {art} alt = "cds"/>
+                            </div>
+
                         </div>
 
                     </div>
 
                     <div className = "col-lg-4 col-xm-12">
 
-                        <div className = "photo-wrap mb-5">
-                            <img className = "merch-item" src = {art} alt = "art paintings"/>
+                        <div className = "box">
+                            <div className = "header">
+                                <h3>Short Courses</h3> 
+                            </div>
+
+                            <div className = "merch-photo-wrap">
+                                <img className = "merch-item" src = {cds} alt = "cds"/>
+                            </div>
+                            
                         </div>
 
-                    </div>
-
-                    <div className = "col-lg-4 col-xm-12">
-
-                        <div className = "photo-wrap mb-5">
-                            <img className = "merch-item" src = {cds} alt = "cds"/>
-                        </div>
+     
 
                     </div>
 
